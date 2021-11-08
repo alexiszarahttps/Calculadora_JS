1dd82b77da4dd6216251c9709ef29ed58f75366c
Alexis A. Zarate <alexisarielzarate@icloud.com>
2021-11-08 14:00:22 -0300

    arr

--- app.js
+++ app.js
@@ -1,59 +1,18 @@
-function num7() {
-    document.calculadora.ans.value+='7'
-}
+const num7 = () => document.calculadora.ans.value += '7'
+const num8 = () => document.calculadora.ans.value += '8'
+const num9 = () => document.calculadora.ans.value += '9'
+const suma = () => document.calculadora.ans.value += '+'
 
-function num8() {
-    document.calculadora.ans.value+='8'
-}
+const num4 = () => document.calculadora.ans.value += '4'
+const num5 = () => document.calculadora.ans.value += '5'
+const num6 = () => document.calculadora.ans.value += '6'
+const resta = () => document.calculadora.ans.value += '-'
 
-function num9() {
-    document.calculadora.ans.value+='9'
-}
+const num1 = () => document.calculadora.ans.value += '1'
+const num2 = () => document.calculadora.ans.value += '2'
+const num3 = () => document.calculadora.ans.value += '3'
+const multiplica = () => document.calculadora.ans.value += '*'
 
-function suma() {
-    document.calculadora.ans.value+='+'
-}
-
-function num4() {
-    document.calculadora.ans.value+='4'
-}
-
-function num5() {
-    document.calculadora.ans.value+='5'
-}
-
-function num6() {
-    document.calculadora.ans.value+='6'
-}
-
-function resta() {
-    document.calculadora.ans.value+='-'
-}
-
-function num1() {
-    document.calculadora.ans.value+='1'
-}
-
-function num2() {
-    document.calculadora.ans.value+='2'
-}
-
-function num3() {
-    document.calculadora.ans.value+='3'
-}
-
-function multiplica() {
-    document.calculadora.ans.value+='*'
-}
-
-function num0() {
-    document.calculadora.ans.value+='0'
-}
-
-function divide() {
-    document.calculadora.ans.value+='/'
-}
-
-function igual() {
-    document.calculadora.ans.value=eval(document.calculadora.ans.value)
-}
+const num0 = () => document.calculadora.ans.value += '7'
+const divide = () => document.calculadora.ans.value += '/'
+const igual = () => document.calculadora.ans.value=eval(document.calculadora.ans.value)
