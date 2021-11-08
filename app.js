4227dbba2b50663c1691add00ddce2d2b56e8178
Alexis A. Zarate <alexisarielzarate@icloud.com>
2021-11-08 13:33:34 -0300

    rdy

--- app.js
+++ app.js
@@ -1,7 +1,59 @@
-function simp() {
+function num7() {
     document.calculadora.ans.value+='7'
 }
 
-function simp() {
+function num8() {
     document.calculadora.ans.value+='8'
+}
+
+function num9() {
+    document.calculadora.ans.value+='9'
+}
+
+function suma() {
+    document.calculadora.ans.value+='+'
+}
+
+function num4() {
+    document.calculadora.ans.value+='4'
+}
+
+function num5() {
+    document.calculadora.ans.value+='5'
+}
+
+function num6() {
+    document.calculadora.ans.value+='6'
+}
+
+function resta() {
+    document.calculadora.ans.value+='-'
+}
+
+function num1() {
+    document.calculadora.ans.value+='1'
+}
+
+function num2() {
+    document.calculadora.ans.value+='2'
+}
+
+function num3() {
+    document.calculadora.ans.value+='3'
+}
+
+function multiplica() {
+    document.calculadora.ans.value+='*'
+}
+
+function num0() {
+    document.calculadora.ans.value+='0'
+}
+
+function divide() {
+    document.calculadora.ans.value+='/'
+}
+
+function igual() {
+    document.calculadora.ans.value=eval(document.calculadora.ans.value)
 }
